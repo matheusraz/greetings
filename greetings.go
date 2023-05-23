251f255ac7729eaf2bcd7d0a1196c0710502c8c3
matheusraz <matheusrazsre@gmail.com>
2023-05-22 21:53:42 -0300

    chore: :zap: return random greetings
    Signed-off-by: matheusraz <matheusrazsre@gmail.com>

--- greetings.go
+++ greetings.go
@@ -3,12 +3,28 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 func Hello(name string) (string, error) {
 	if name == "" {
 		return "", errors.New("Empty Name")
 	}
-	message := fmt.Sprintf("Hey you dumb, %v! How you doing?", name)
+	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil
+}
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func randomFormat() string {
+	phrases := []string{
+		"E ai %v! Como vai o estudo de golang?",
+		"Eh muita confusao na StackSpot viu, %v.. Nao tem quem aguente!",
+		"Fala %v! Quando tu vai jantar?",
+	}
+
+	return phrases[rand.Intn(len(phrases))]
 }
